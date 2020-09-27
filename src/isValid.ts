e80afed9c52250df39b03c6e52f1ede8a42b2592
Radoslaw Medryk <rame@trustpilot.com>
2020-09-27 20:01:23 +0200

    Changed isValid to have input is TOut return signature

--- src/isValid.ts
+++ src/isValid.ts
@@ -2,7 +2,7 @@ import { Verificable } from "./asVerificable";
 import { Predicate } from "./Predicate";
 import { getErrors } from "./getErrors";
 
-export function isValid<TBase, TOut>(input: Verificable<TBase> | unknown, predicate: Predicate<TOut>): boolean {
+export function isValid<TBase, TOut>(input: Verificable<TBase> | unknown, predicate: Predicate<TOut>): input is TOut {
     const errors = getErrors(input, predicate);
     return errors.length === 0;
 }
